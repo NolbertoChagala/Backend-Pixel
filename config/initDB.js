a6b998d98ec6f638779b17d36cc3c0196df18e48
NolbertoChagala <21393137@utcancun.edu.mx>
2025-07-04 01:17:21 -0500

    Configuración de produccion

--- config/initDB.js
+++ config/initDB.js
@@ -2,16 +2,23 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 async function initializeDatabase() {
+  if (process.env.MYSQL_URL) {
+    const connection = await mysql.createConnection(process.env.MYSQL_URL);
+    console.log('Base de datos conectada correctamente con MYSQL_URL ✅');
+    return connection;
+  }
+
+  // Fallback si no existe MYSQL_URL
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
   });
 
-  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``);
-  console.log('Base de datos verificada o creada ✅');
-
-  await connection.end();
+  console.log('Base de datos conectada correctamente con variables separadas ✅');
+  return connection;
 }
 
 module.exports = initializeDatabase;
